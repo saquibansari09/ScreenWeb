a88580a04cb3d3b3f7efde918c53cbeeac95e694
saquibansari99 <saquib9698@gmail.com>
2023-05-24 11:20:16 +0530

    first commit

--- src/Components/Pages/Alter.js
+++ src/Components/Pages/Alter.js
@@ -4,33 +4,32 @@ import { BsCurrencyRupee } from "react-icons/bs";
 const Alter = () => {
   return (
     <section className="">
-      <div className="bg-sky-600 h-[40px] w-[100%]">
-        <h1 className="text-2xl font-bold px-5">Company Alteration</h1>
+      <div className="w-[100%] h-[25px] bg-sky-400 ">
+        <h1 className="text-base ml-3">Company Creation</h1>
       </div>
-      <div className="flex lg:flex justify-start gap-10">
-        <div className="flex gap-7 mt-5">
-          <label className="mt-2 ml-3 text-sm">Company Name:</label>
-          <input className="bg-gray-200  appearance-none border-2   py-2 px-4 text-gray-700  focus:outline-none focus:bg-yellow-400 focus:border-purple-500" />
-        </div>
-        <div className="mt-5">
-          <p className="">
-            Financial year beginning from
-            <br /> Books beginning from
-          </p>
-        </div>
-        <div className="text-sm font-bold mt-5">
-          <p>
-            :1-Apr-23
-            <br />
-            :1-Apr-23
-          </p>
-        </div>
+
+      <div className="flex flex-row text-justify gap-10 mt-5">
+        <label className="ml-3  text-sm mt-3">Company Name:</label>
+        <input className="bg-gray-200   appearance-none border-2   py-2 px-4 focus:outline-none focus:bg-yellow-400 focus:border-sky-400" />
+        <p>
+          Financial year beginning from <br /> Books beginning from
+        </p>
+        <p>
+          :1.Apr-23
+          <br /> :1.Apr.23
+        </p>
       </div>
+
+      <div className="flex gap-7 mt-5">
+        <label className="mt-2 ml-3 text-sm">Company Name:</label>
+        <input className="bg-gray-200  appearance-none border-2 text-black   py-2 px-4 focus:outline-none focus:bg-yellow-400 focus:border-sky-400" />
+      </div>
+
       <div className=" flex gap-10 mt-5">
         <label className="mt-2 text-sm ml-3">Mailing Name:</label>
         <input
           type="text"
-          className="bg-gray-200 appearance-none border-2 border-sky-400 py-2 px-4 text-gray-700  focus:outline-none focus:bg-yellow-400 focus:border-sky-400"
+          className="bg-gray-200 appearance-none border-2  py-2 px-4 text-gray-700  focus:outline-none focus:bg-yellow-400 focus:border-sky-400"
         />
       </div>
       <div className="flex gap-20 mt-5">
@@ -58,28 +57,28 @@ const Alter = () => {
         <label className="mt-2 text-sm ml-3">Mobile:</label>
         <input className="bg-gray-200  appearance-none border-2 py-2 px-4 text-gray-700  focus:outline-none focus:bg-yellow-400 focus:border-sky-400" />
       </div>
-      <div className=" mt-5 flex gap-24">
+      <div className=" mt-5 flex gap-28">
         <label className="mt-2 text-sm ml-3">Fax:</label>
         <input className="bg-gray-200  appearance-none border-2 py-2 px-4 text-gray-700  focus:outline-none focus:bg-yellow-400 focus:border-sky-400" />
       </div>
-      <div className="mt-5 flex gap-20">
+      <div className="mt-5 flex gap-24">
         <label className="mt-2 text-sm ml-3">E-mail:</label>
         <input className="bg-gray-200 appearance-none border-2 py-2 px-4 text-gray-700  focus:outline-none focus:bg-yellow-400 focus:border-sky-400" />
       </div>
-      <div className="mt-5 flex gap-16">
+      <div className="mt-5 flex gap-20">
         <label className="mt-2 text-sm ml-3">Website:</label>
         <input className="bg-gray-200 appearance-none border-2 py-2 px-4 text-gray-700  focus:outline-none focus:bg-yellow-400 focus:border-sky-400" />
       </div>
       <hr className="" />
-      <div className="flex gap-10 px-20 text-xl">
+      <div className="flex gap-10  text-sm ml-3">
         <p>Base Curruncy symbole:</p>
         <span>
           {" "}
           <BsCurrencyRupee className="text-2xl" />
         </span>
       </div>
-      <div className="flex gap-10 text-xl">
-        <p className="text-xl pl-20">Formal name:</p>
+      <div className="flex gap-10 text-sm ml-3">
+        <p className="text-sm ">Formal name:</p>
         <span>INR</span>
       </div>
     </section>
