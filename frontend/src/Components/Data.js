78c48e644d3dd64463caecc219c2b87cac80f54a
saquibansari99 <105703040+saquibansari98@users.noreply.github.com>
2023-07-13 01:45:02 +0530

    some changes

--- frontend/src/Components/Data.js
+++ frontend/src/Components/Data.js
@@ -8,74 +8,102 @@ import { DataGrid } from "@mui/x-data-grid";
 const Data = () => {
   const [open, setOpen] = useState([]);
 
-  const data = async () => {
-    const res = await axios.get("http://localhost:5000/storeAlter");
+  // const data = async () => {
+  //   const res = await axios.get("http://localhost:5000/storeAlter");
 
-    setOpen(res.data);
-  };
-  useEffect(() => {
-    data();
-  }, []);
+  //   setOpen(res.data);
+  // };
+  // useEffect(() => {
+  //   data();
+  // }, []);
 
   const columns = [
     { field: "id", headerName: "ID", width: 90 },
     {
-      field: "firstName",
-      headerName: "Companyname",
+      field: "companyname",
+      headerName: "Company Name",
       width: 150,
-      editable: true,
-    },
-    {
-      field: "lastName",
-      headerName: "Mailingname",
-      width: 150,
-      editable: true,
     },
 
     {
-      field: "lastName",
-      headerName: "address",
+      field: "mailingname",
+      headerName: "Mailing Name",
       width: 150,
-      editable: true,
     },
 
     {
-      field: "lastName",
-      headerName: "Mailingname",
-      width: 150,
-      editable: true,
-    },
-
-    {
-      field: "lastName",
+      field: "address",
       headerName: "Address",
       width: 150,
+    },
+
+    {
+      field: "state",
+      headerName: "State",
+      width: 150,
       editable: true,
     },
 
     {
-      field: "fullName",
-      headerName: "Full name",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+      field: "country",
+      headerName: "Country",
+      width: 150,
+      editable: true,
+    },
+
+    {
+      field: "pincode",
+      headerName: "Pincode",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "telephone",
+      headerName: "Telephone",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "mobile",
+      headerName: "Mobile",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "fax",
+      headerName: "Fax",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "website",
+      headerName: "website",
+      width: 150,
+      editable: true,
     },
   ];
 
   const rows = [
-    { id: 1, companyname: "viser" },
-    { id: 2, lastName: "Lannister", firstName: "Cersei" },
-    { id: 3, address: "ghansoli" },
-    { id: 4, state: "Maharashtra" },
-    { id: 5, country: "India" },
-    { id: 6, telephone: "68836464783" },
-    { id: 7, mobile: "8767636346" },
-    { id: 8, fax: "fax@gmai.com" },
-    { id: 9, email: "vdhdh@gmail.com" },
-    { id: 9, website: "hgfhfh" },
-    { id: 9, pincode: "4000701" },
+    {
+      id: 1,
+      companyname: "viser",
+      mailingname: "bla bla",
+      address: "ghansoli",
+      state: "Maharashtra",
+      country: "India",
+      telephone: "68836464783",
+      mobile: "8767636346",
+      fax: "fax@gmai.com",
+      email: "vdhdh@gmail.com",
+      website: "hgfhfh",
+      pincode: "4000701",
+    },
   ];
 
   console.log(open, "ooooooooooo");
